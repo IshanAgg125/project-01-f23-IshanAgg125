81f2a6de3775fcecdff4301e81c901744e889a8b
IshanAgg125 <tuo10683@temple.edu>
2023-10-12 23:58:29 -0400

    All final changes have been made

--- MYps.c
+++ MYps.c
@@ -30,8 +30,21 @@ char **getSameUserID();
 char *printProcessInfo(char *PID);
 struct proc getAllTheInformation(char* PIDpath, int argc, char **argv);
 struct proc informationOfSameUserID(char *PID, int argc, char **argv);
+//bool valueAfterPIsAnInteger(char *argument);
 
+/*
 
+The main function is taking arguments from the user
+If the argument contains -p, then I am saving the element after it and saving it in PIDpath
+If contains -p I am going inside getAllTheInformation() which returns a proc based on argument given.
+If the arguments does not contain -p I am calling the getSameUserID()
+This function is returning a pointer to a pointer, aka a 2D array. 
+This array contains all the same PID.
+Then I am looping through the array and sending each of the PID into informationOfSameUserID
+Which returns a proc and then I saving that into a proc.
+Lastly, I am calling the printElementsOfProc(), which takes proc as a parameter.
+
+*/ 
 
 int main(int argc, char **argv) {
     int locationOfPID = 0;
@@ -62,6 +75,113 @@ int main(int argc, char **argv) {
     return 0;
 }
 
+/*
+This is the function I am calling if (argv) contains -p. 
+The function return a struct proc. The proc is saving all the information based on the argv. 
+For example, if there is an argument, I am calling the function accordingly
+If argument contains:
+-s then stateInformation
+-S then systemTime
+-v then virtual memory
+If the arguments (argv) does not contain one of the elements I am setting the value 0 or "\0"
+accordingly. 
+In the end I am returning the proc and then I am printingAllTheElementsOfProc
+
+*/
+
+struct proc getAllTheInformation(char* PIDpath, int argc, char **argv) {
+    int containsUtime = 0;
+    int containsCommandLine = 0;
+    int option;
+    struct proc myProc;
+    myProc.pid = atoi(PIDpath);
+
+    bool si = false;
+    bool st = false;
+    bool vm = false;
+
+    while ((option = getopt(argc, argv, "sSUvcp")) != -1) {
+        //printf("%s %c\n", "option = ", option);
+        switch(option) {
+            case 's':
+                //printf("[%d] PID is = %s\n",locationOfPID ,PIDpath);
+                char result = stateInformation(PIDpath);
+                //printf("result -s: %c\n", result);
+                myProc.state = result;
+                si = true;
+                break;
+            case 'S':
+                //printf("[%d] PID is = %s\n",locationOfPID ,PIDpath);
+                int resultS = systemTime(PIDpath);
+                //printf("result -S: %d\n", resultS);
+                myProc.stime = resultS;
+                st = true;
+                break;
+            case 'v':
+                //printf("[%d] PID is = %s\n",locationOfPID ,PIDpath);
+                int resultV = virtualMemory(PIDpath);
+                //printf("result -v: %d\n", resultV);
+                myProc.vMemory = resultV;
+                vm = true;
+                break;
+            case 'U':
+                //printf("[%d] PID is = %s\n",locationOfPID ,PIDpath);
+                printf("%s\n", "Entering U time");
+                containsUtime = 1;
+                break;
+            case 'c':
+                //printf("[%d] PID is = %s\n",locationOfPID ,PIDpath);
+                containsCommandLine = 1;
+                break;
+            case 'p':
+                continue;
+            default:
+                fprintf(stderr, "Usage: %s [-p PID] [-s] [-S] [-v] [-U] [-c]\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    if (!si) {
+        myProc.state = '\0';
+    }
+    if (!st) {
+        myProc.stime = -1;
+    }
+    if (!vm) {
+        myProc.vMemory = -1;
+    }
+
+    if (!containsUtime) {
+        //printf("%s\n", "Entering U time part 2");
+        int result = userTime(PIDpath);
+        myProc.utime = result;
+    } else {
+        myProc.utime = -1;
+    }
+
+    if (!containsCommandLine) {
+        char *result = commandline(PIDpath);
+        strncpy(myProc.command, result, sizeof(myProc.command));
+        free(result);
+    }
+    return myProc;
+}
+
+/*
+Information of same user ID take a char *PID, number of arguments, and all the argv arguments
+from the user.
+If the argc is 1, that means only MYps is the argument. If that is the case, I am printing 
+the commandLine and the userTime. The rest of the value in the proc are being set to -1 or "\0".
+Else if there is some more argument, I am calling the function accordingly
+If argument contains:
+-s then stateInformation
+-S then systemTime
+-v then virtual memory
+If the arguments (argv) does not contain one of the elements I am setting the value 0 or "\0"
+accordingly. 
+Lastly, I am returning a proc. 
+
+*/
 
 struct proc informationOfSameUserID(char *PID, int argc, char **argv) {
     //printf("%s\n", "Entering teh function");
@@ -137,6 +257,11 @@ struct proc informationOfSameUserID(char *PID, int argc, char **argv) {
     return myProc;
 }
 
+/*
+This process checks if the argument provided is -p. Using it in the top to loop through all argv.
+If contains then returns true, else false.
+*/
+
 bool containsProcessInformation(char *argument) {
     if (strcmp(argument, "-p") == 0) {
         return true;
@@ -144,66 +269,11 @@ bool containsProcessInformation(char *argument) {
     return false;
 }
 
-struct proc getAllTheInformation(char* PIDpath, int argc, char **argv) {
-    int containsUtime = 0;
-    int containsCommandLine = 0;
-    int option;
-    struct proc myProc;
-    myProc.pid = atoi(PIDpath);
+/*
 
-    while ((option = getopt(argc, argv, "sSUvcp")) != -1) {
-        printf("%s %c\n", "option = ", option);
-        switch(option) {
-            case 's':
-                //printf("[%d] PID is = %s\n",locationOfPID ,PIDpath);
-                char result = stateInformation(PIDpath);
-                //printf("result -s: %c\n", result);
-                myProc.state = result;
-                break;
-            case 'S':
-                //printf("[%d] PID is = %s\n",locationOfPID ,PIDpath);
-                int resultS = systemTime(PIDpath);
-                //printf("result -S: %d\n", resultS);
-                myProc.stime = resultS;
-                break;
-            case 'v':
-                //printf("[%d] PID is = %s\n",locationOfPID ,PIDpath);
-                int resultV = virtualMemory(PIDpath);
-                //printf("result -v: %d\n", resultV);
-                myProc.vMemory = resultV;
-                break;
-            case 'U':
-                //printf("[%d] PID is = %s\n",locationOfPID ,PIDpath);
-                printf("%s\n", "Entering U time");
-                containsUtime = 1;
-                break;
-            case 'c':
-                //printf("[%d] PID is = %s\n",locationOfPID ,PIDpath);
-                containsCommandLine = 1;
-                break;
-            case 'p':
-                continue;
-            default:
-                fprintf(stderr, "Usage: %s [-p PID] [-s] [-S] [-v] [-U] [-c]\n", argv[0]);
-                exit(EXIT_FAILURE);
-        }
-    }
-
-    if (!containsUtime) {
-        printf("%s\n", "Entering U time part 2");
-        int result = userTime(PIDpath);
-        myProc.utime = result;
-    } else {
-        myProc.utime = -1;
-    }
-
-    if (!containsCommandLine) {
-        char *result = commandline(PIDpath);
-        strncpy(myProc.command, result, sizeof(myProc.command));
-        free(result);
-    }
-    return myProc;
-}
+int virtualMemory returns the virtual memory of the PID. I am open the file based on the path
+From there I am 
+*/
 
 int virtualMemory(char *PID) {
     FILE *file;
@@ -234,6 +304,15 @@ int virtualMemory(char *PID) {
     return result;
 }
 
+/*
+the stateInformation contains PID as the argument. 
+I am opening the file and then looping through the file
+Looking at the manual of stat, I saw that the third paramater is the state information.
+Furthermore, I am saving that char in processsState, and then returning stateInformation. 
+
+
+*/
+
 char stateInformation(char *PID) {
     FILE *file;
     char pathName[200];
@@ -261,6 +340,16 @@ char stateInformation(char *PID) {
     fclose(file);
     return processState;
 }
+
+/*
+the userTime contains PID as the argument. 
+I am opening the file and then looping through the file
+Looking at the manual of stat, I saw that the 14th paramater is the user time information.
+Furthermore, I am saving that int in result, and then returning result.
+
+
+
+*/
 
 int userTime(char *PID) {
     FILE *file;
@@ -291,6 +380,14 @@ int userTime(char *PID) {
     return result;
 }
 
+/*
+the systemTime contains PID as the argument. 
+I am opening the file and then looping through the file
+Looking at the manual of stat, I saw that the 15th paramater is the system time information.
+Furthermore, I am saving that int in result, and then returning result.
+ 
+*/
+
 int systemTime(char *PID) {
     FILE *file;
     char pathName[200];
@@ -320,6 +417,17 @@ int systemTime(char *PID) {
     fclose(file);
     return result;
 }
+
+/*
+commandLine takes PID as an argument. 
+Next I am opening the file and then looping though the contents of it. 
+Next I am reallocating memory by size + 2
+The result is being saved in a temporary pointer 
+Then toReturn point to temp which is the newly allocated memory
+And then save the charcater read from toReturn at size
+And then I finally increase the size. 
+
+*/
 
 char *commandline(char *PID) {
     FILE *file;
@@ -354,6 +462,13 @@ char *commandline(char *PID) {
     return toReturn;
 }
 
+/*
+This is probably the most important function. 
+It prints the values of the proc. 
+
+
+*/
+
 void printElementsOfStruct(struct proc myProc) {
     printf("%s", "Output: ");
     if (myProc.pid != -1) {
@@ -377,6 +492,24 @@ void printElementsOfStruct(struct proc myProc) {
     printf("\n");
 }
 
+// bool valueAfterPIsAnInteger(char *argument) {
+//     char *ptrAtTheEnd; 
+//     int value = strtol(argument, &ptrAtTheEnd, 10);
+//     if (*ptrAtTheEnd == "\0") {
+//         return true;
+//     }
+// }
+
+/*
+The printProcessInfo finds same user ID as mine
+First I open the file and then I am looping through the file. 
+I am saving the entire line and then using sscanf
+The usage of sscanf allows me to save what is after the "UID" which is the user id
+Next, I compare the userID with mine.
+If it is a match, then I am returning the PID;
+If not then return Null.
+
+*/
 
 char *printProcessInfo(char *PID) {
 
@@ -404,6 +537,17 @@ char *printProcessInfo(char *PID) {
     fclose(file);
     return NULL;
 }
+
+/*
+The getSameUserID function returns a pointer to the first element of the char *
+I am opening the /proc directory. 
+Next, I am rading from the directory and then I am saving each entry in entryName
+Next I am checking if the entryName is a directory
+And then I am sending each directory to printProcessInfo which return PID is the UID is same.
+And then I am saving it in toReturn.
+Finally closing directory and then retuning toReturn.
+
+*/
 
 char **getSameUserID() {
     DIR *dirp;
